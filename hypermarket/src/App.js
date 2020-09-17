5508a7309c7c6b6856764e117878f1ea18d4018e
Sufyan Abbada <62093556+SufyanAbbada@users.noreply.github.com>
2020-09-17 18:56:19 +0500

    Update App.js

--- hypermarket/src/App.js
+++ hypermarket/src/App.js
@@ -35,7 +35,7 @@ function App() {
   return (
     <Router>
       <div>
-        <div style={{ paddingLeft: "177px" }}>
+        <div style={{ paddingLeft: "195px" }}>
           <ToastContainer />
           <TopMenue />
 
